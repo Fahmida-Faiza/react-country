875b4bf19953cdb67a7ffad297d0eca4c84db439
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-09-10 17:04:28 +0600

    added visited flags

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -7,6 +7,11 @@ const Countries = () => {
 
 const [countries, setCountries] =useState([]);
 
+const [visitedCountries, setVisitedCountries] =useState([]);
+// step1:
+const [visitedFlags, setVisitedFlags]=useState([]);
+
+
 useEffect(()=>{
 fetch('https://restcountries.com/v3.1/all')
 .then(res => res.json())
@@ -15,15 +20,62 @@ fetch('https://restcountries.com/v3.1/all')
 
 }, [])
 
+const handleVisitedCountry = country =>{
+    console.log('add to your country visited')
+    // console.log(country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+}
+
+// step2:evenhandler add
+const handleVisitedFlags= flag =>{
+
+    // console.log('flag adding')
+    // step6:new array create korey agergula soho copy korey
+    const newVisitedFlags= [...visitedFlags, flag]
+setVisitedFlags(newVisitedFlags);
+}
+
 
 
     return (
+        // visited country
         <div>
            <h3>Countries: {countries.length}</h3> 
+        <div>
+        <h5>Visited Countries: {visitedCountries.length}</h5>
+        <ul>
+        {
+
+            visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+        }
+
+        </ul>
+       
+
+
+           </div>
+ {/* step7:show korbo flags gula */}
+ <div className="flag-container">
+
+{
+    visitedFlags.map(flag => <img src={flag}></img>)
+}
+
+
+ </div>
+
+
+           {/* display countries */}
           <div className="country-container">
              {
                  countries.map(country => <Country
-                 key={country.cca3} country={country}></Country>)
+                 key={country.cca3} 
+                 handleVisitedCountry={handleVisitedCountry}
+                //  step3:pathabo
+                handleVisitedFlags={handleVisitedFlags}
+                 
+                 country={country}></Country>)
            }
           </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,32 @@
 
+import { useState } from 'react';
 import './Country.css'
-
-const Country = ({country}) => {
+// step4:distructure props pabo handleVisitedFlags agula..
+const Country = ({country , handleVisitedCountry , handleVisitedFlags}) => {
     // console.log(country)
-    const {name, flags, population, area} =country;
+    const {name, flags, population, area, cca3} =country;
+
+    const [visited, setVisited] = useState(false)
+    const handleVisited =()=>{
+        setVisited(!visited);
+    }
+
+    // console.log(handleVisitedCountry);
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <h3>Name:{name?.common} </h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
+            {/* step5:paramiter pathabo array tey */}
+            <button onClick={()=> handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
+            {/* {visited && 'I have visite this place'} */}
+            {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
     );
 };
